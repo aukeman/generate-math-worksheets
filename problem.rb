b285b1af6033b1166f0e99802ffb521eab0fe2d4
Jacob Aukeman <jacob@jacobaukeman.com>
2016-04-13 22:36:48 -0400

    additional constraints on answers

--- problem.rb
+++ problem.rb
@@ -2,6 +2,8 @@ require_relative 'override_equality'
 
 class Problem
 
+  OPS = { '+' => proc{|a,b| a+b}, '-' => proc{|a,b| a-b} }
+
   include OverrideEquality
 
   attr_reader :terms, :operation
@@ -9,6 +11,10 @@ class Problem
   def initialize( terms = [0,0], operation = '+' )
     @terms=terms
     @operation=operation
+  end
+
+  def answer
+    @terms.reduce(&OPS[@operation])
   end
 
   def to_json
--- problem_factory.rb
+++ problem_factory.rb
@@ -3,14 +3,25 @@ require_relative 'problem_history'
 
 class ProblemFactory
   
-  attr_accessor :history, :addition, :subtraction, :maximum, :maximum_on_top, :prng
+  attr_accessor :history
+  attr_accessor :addition 
+  attr_accessor :subtraction
+  attr_accessor :maximum_term
+  attr_accessor :minimum_term
+  attr_accessor :greater_term_on_top
+  attr_accessor :maximum_answer
+  attr_accessor :minimum_answer
+  attr_accessor :prng
 
   def initialize &block
     self.history=10
     self.addition=true
     self.subtraction=true
-    self.maximum=10
-    self.maximum_on_top=true
+    self.maximum_term=10
+    self.minimum_term=0
+    self.greater_term_on_top=true
+    self.maximum_answer=999
+    self.minimum_answer=-999
     self.prng=Random.new
     configure &block
   end
@@ -26,13 +37,24 @@ class ProblemFactory
 
   def build
     begin
-      a,b=prng.rand(maximum), prng.rand(maximum)
-      b,a=a,b if maximum_on_top && a < b
+      a,b=generate_term, generate_term
+      b,a=a,b if greater_term_on_top && a < b
       op=@operations.sample(random: prng)
       problem=Problem.new([a,b], op) 
-    end while @problem_history.has? problem
+    end while constraints_unsatisfied_by problem 
     @problem_history.add problem
     problem
   end
+
+  def constraints_unsatisfied_by problem
+    (@problem_history.has?(problem) ||
+     problem.answer < minimum_answer ||
+     maximum_answer < problem.answer)
+  end
+
+  def generate_term
+    prng.rand(maximum_term+1-minimum_term) + minimum_term
+  end
+
 end
 
